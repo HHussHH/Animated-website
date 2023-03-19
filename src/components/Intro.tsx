75ffdef9063ada8f6d1ea7bc2f80b0d2fc0ab08d
Nariman <abonent26pg@mail.ru>
2023-03-19 11:27:22 +0300

    fixed old animate and add scroll animate

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -5,89 +5,105 @@ import { ImGithub } from "react-icons/im";
 import { TbGridDots } from "react-icons/tb";
 import { FaTelegram } from "react-icons/fa";
 
-import { motion } from "framer-motion";
-
-interface IAnimate {
-  y: number[];
-  scale: number[];
-}
-
-interface Itransform {
-  duration?: number;
-}
-const animate: IAnimate = {
-  y: [200, 0],
-  scale: [0, 1],
-};
-const transition: Itransform = {
-  duration: 1,
-};
+import { motion, Variants } from "framer-motion";
 
 const Intro = () => {
+  const textAnimation: Variants = {
+    hidden: {
+      y: 300,
+      opacity: 0,
+    },
+    visible: (custom: number) => ({
+      y: 0,
+      opacity: 1,
+      transition: {
+        delay: custom,
+        duration: 1,
+      },
+    }),
+  };
+
+  const buttomAnimate: Variants = {
+    hidden: (custom: number) => ({
+      y: 100,
+      x: custom,
+      opacity: 0,
+    }),
+    visible: {
+      y: 0,
+      x: 0,
+      opacity: 1,
+      transition: {
+        delay: 1,
+        duration: 1,
+      },
+    },
+  };
+
+  const iconsAnimate: Variants = {
+    hidden: {
+      scale: 0,
+      opacity: 0,
+    },
+    visible: {
+      scale: 1,
+      opacity: 1,
+      transition: {
+        delay: 1,
+        duration: 1.5,
+      },
+    },
+  };
+
   return (
-    <div className={styles.bg}>
-      <motion.h1
-        animate={animate}
-        transition={transition}
-        className={styles.title}
-      >
+    <motion.div
+      initial="hidden"
+      viewport={{ amount: 0.4, once: true }}
+      whileInView="visible"
+      className={styles.bg}
+    >
+      <motion.h1 variants={textAnimation} custom={0} className={styles.title}>
         Analytics for <br />
         <span>developers</span>
       </motion.h1>
       <motion.h2
-        animate={animate}
-        transition={transition}
+        variants={textAnimation}
+        custom={0.5}
         className={styles.subtitle}
       >
         Easy to install. Unlimited power.
       </motion.h2>
       <div className={styles.btnGroup}>
         <motion.button
+          variants={buttomAnimate}
+          custom={-100}
           className={styles.btn}
-          animate={{
-            y: [70, 0],
-            x: [-70, 0],
-            scale: [0, 1],
-          }}
-          transition={transition}
         >
           Get started
         </motion.button>
         <motion.button
+          variants={buttomAnimate}
+          custom={100}
           className={styles.btn}
-          animate={{
-            y: [70, 0],
-            x: [70, 0],
-            scale: [0, 1],
-          }}
-          transition={transition}
         >
           Book Demo
         </motion.button>
       </div>
       <div className={styles.partners}>
         <motion.p
+          variants={textAnimation}
+          custom={0.4}
           className={styles.partnersTitle}
-          animate={{
-            y: [100, 0],
-            opacity: [0, 1],
-          }}
-          transition={{ duration: 1 }}
         >
           TRUSTED BY
         </motion.p>
         <motion.div
+          initial="hidden"
+          viewport={{ amount: 0.4, once: true }}
+          whileInView="visible"
+          variants={iconsAnimate}
+          custom={0}
           className={styles.icons}
-          animate={{
-            y: [50, 0],
-            x: [20, 0],
-            scale: [0.1, 1],
-            opacity: [0, 1],
-          }}
-          transition={{
-            delay: 0.5,
-            duration: 1,
-          }}
         >
           <FaTelegram size={60} />
           <TbGridDots color="white" size={60} />
@@ -96,7 +112,7 @@ const Intro = () => {
           <IoLogoVenmo size={60} />
         </motion.div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
